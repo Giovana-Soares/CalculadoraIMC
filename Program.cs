2faff08b61fdf4386794d91a72a292d8ebfc1ce7
Giovana <giihs2005@gmail.com>
2022-03-29 14:31:30 -0300

    Recebe dados, calcula IMC e apresenta ao usuario

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace CalculadoraIMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Qual seu nome?");
+            string nome = Console.ReadLine();
+            
+            Console.WriteLine("Qual sua altura?");
+            float altura = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Qual seu peso?");
+            float peso = Convert.ToSingle(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+            Console.WriteLine("{0} sua altura é {1} e seu peso é {2}", nome, altura, peso);
+            Console.WriteLine("Seu IMC: " +imc);
+
+        
+            
         }
     }
 }
